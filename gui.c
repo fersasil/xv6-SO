d7f6994b61457643360da16748aeade2b7012431
Fersasil <guilhermefersasil@gmail.com>
2019-09-29 14:13:13 -0300

    algoritmo de teste atualizado

--- gui.c
+++ gui.c
@@ -2,9 +2,44 @@
 #include "stat.h"
 #include "user.h"
 
+volatile void take_time(){
+    // volatile int l;
+    // unsigned long long int gui = 0;
+    // gui++;
+    
+    // for(int i = 0; i < 1000000; i++)
+    //     for(int j = 0; j < 100000; j++)
+    //         gui++;
+    
+    // printf(1, "fim %d\n", gui);
+
+    // sleep(500);
+    sleep(1000);
+    printf(1, "ok\n");
+
+}
+
 int main(){
+    int i;
     printf(1, "Ola mundo\n");
-    fork(50);
+    
+    for(i = 0; i < 5; i++){
+        int pid = fork(50);
+        if(pid > 0){
+        //É o filho!
+            
+            printf(1, "Eu sou o filho!\n");
+
+            take_time();
+            
+            wait();
+        }
+        else if(pid == 0){
+            exit(); //É o pai
+        }
+    }
+
+    
 
 
     exit();
