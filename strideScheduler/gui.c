a64d8c3c84ca5710f250fa1c41cf99c76f0799cc
Fersasil <guilhermefersasil@gmail.com>
2019-11-07 20:34:32 -0300

    Stride scheduler first commit

---
+++ strideScheduler/gui.c
@@ -0,0 +1,52 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define N  5
+
+void
+forktest(void)
+{
+  int n, pid;
+
+  printf(1, "fork test\n");
+
+  for(n=0; n<N; n++){
+    if(n == 0)
+      pid = fork(50); // Primeiro processo ganha 50 tickets
+    else{
+      pid = fork(10); // Os outros ganham 10 tickets!
+    }
+    if(pid < 0)
+      break;
+    if(pid == 0)
+      for(; ; ); // Loop infinito
+  }
+
+//   if(n == N){
+//     printf(1, "fork claimed to work %d times!\n", N);
+//     exit();
+//   }
+
+  for(; n > 0; n--){
+    if(wait() < 0){
+      printf(1, "wait stopped early\n");
+      exit();
+    }
+  }
+
+  if(wait() != -1){
+    printf(1, "wait got too many\n");
+    exit();
+  }
+
+  printf(1, "fork test OK\n");
+  exit();
+}
+
+int
+main(void)
+{
+  forktest();
+  exit();
+}
---
+++ strideScheduler/zombie.c
@@ -0,0 +1,15 @@
+// Create a zombie process that
+// must be reparented at exit.
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "stride_scheduler.h"
+
+int
+main(void)
+{
+  if(fork(TICKETS_DEFAULT) > 0)
+    sleep(5);  // Let child exit before parent.
+  exit();
+}
